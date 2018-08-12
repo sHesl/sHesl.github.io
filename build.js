a507b56332f7f8eb5dc36dd393043f857975052f
sHesl <sHesl@users.noreply.github.com>
2018-08-12 17:44:38 +0100

    samheslop.com

---
+++ build.js
@@ -0,0 +1,32 @@
+const showdown = require('showdown');
+const fs = require('fs');
+
+const converter = new showdown.Converter();
+
+const mdDir = './md/';
+const postDir = './posts/';
+
+const contentPlaceholder = '{{CONTENT}}';
+const template = fs.readFileSync('template.html');
+
+fs.readdir(mdDir, (err, filenames) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  filenames.forEach((filename) => {
+    fs.readFile(mdDir + filename, 'utf-8', (err, content) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const postHtml = converter.makeHtml(content);
+      const post = template.toString().replace(contentPlaceholder, postHtml);
+      const postFileName = filename.replace('.md', '.html');
+      fs.writeFile(postDir + postFileName, post, console.error);
+    });
+  });
+});
+
